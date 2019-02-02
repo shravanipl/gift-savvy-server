8433f9b213bfcf57b04af2bc0ab269383e7b8da4
shravanipl <pl.shravani@gmail.com>
2019-02-01 20:03:15 -0600

    User Registration issuefix

--- users/models.js
+++ users/models.js
@@ -25,7 +25,7 @@ const userSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
-        unique: [true],
+        unique: true,
         minlength: 4,
         maxlength:15
     },
